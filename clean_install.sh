04c723b5d710180ce6afd311c2f75c15a2dd1602
Daniel <60440770+AgniaInBloom@users.noreply.github.com>
2024-01-25 12:05:05 +0200

    Update clean_install.sh

--- clean_install.sh
+++ clean_install.sh
@@ -12,7 +12,7 @@ cd /home/oracle
 
 cat /dev/null > vi .bash_profile
 
-cat > /etc/yum.repos.d/docker.repo <<EOF 
+cat > .bash_profile <<EOF 
 ORACLE_BASE=/u01/app/oracle; export ORACLE_BASE
 ORACLE_HOME=$ORACLE_BASE/product/12.2.0.1/dbhome_1; export ORACLE_HOME
 ORACLE_SID=invoice; export ORACLE_SID
@@ -34,4 +34,52 @@ if [ $USER = "oracle" ]; then
         fi
 fi 
 EOF
+
+. .bash_profile
+
+cd /tmp
+wget  ##Ссылка на архив исходника
+
+mkdir /u01/app/oracle/product/12.2.0.1/dbhome_1
+cd /u01/app/oracle/product/12.2.0.1/dbhome_1
+
+unzip -qo /tmp/V839960-01.zip
+cd $ORACLE_HOME/database
+
+###
+
+./runInstaller -ignoreSysPrereqs -showProgress -silent       \
+oracle.install.option=INSTALL_DB_SWONLY                      \
+ORACLE_HOSTNAME=${HOSTNAME}                                  \
+UNIX_GROUP_NAME=oinstall                                     \
+INVENTORY_LOCATION=/u01/app/oraInventory                     \
+SELECTED_LANGUAGES=en,en_GB                                  \
+ORACLE_HOME=${ORACLE_HOME}                                   \
+ORACLE_BASE=${ORACLE_BASE}                                   \
+oracle.install.db.InstallEdition=EE                          \
+oracle.install.db.OSDBA_GROUP=dba                            \
+oracle.install.db.OSOPER_GROUP=dba                           \
+oracle.install.db.OSBACKUPDBA_GROUP=dba                      \
+oracle.install.db.OSDGDBA_GROUP=dba                          \
+oracle.install.db.OSKMDBA_GROUP=dba                          \
+oracle.install.db.OSRACDBA_GROUP=dba                         \
+SECURITY_UPDATES_VIA_MYORACLESUPPORT=false                   \
+DECLINE_SECURITY_UPDATES=true                                \
+oracle.installer.autoupdates.option=SKIP_UPDATES
+
+###
+
+su root 
+pandora
+
+sh /u01/app/oraInventory/orainstRoot.sh
+/u01/app/oracle/product/12.2.0.1/dbhome_1/root.sh
+
+su oracle 
+oracle4u
+
+cd /u01/app/oracle/product/12.2.0.1/dbhome_1/bin
+./dbca -silent -createDatabase                   -templateName General_Purpose.dbc              -gdbName ${ORACLE_SID}                         -sid ${ORACLE_SID}                             -createAsContainerDatabase false               -emConfiguration NONE                          -datafileDestination /u01/db_files             -storageType FS                                -characterSet AL32UTF8                         -totalMemory 2048                              -recoveryAreaDestination /u01/FRA              -sampleSchema true
+
+
 
