a8bf2b410b838abcae00d4504398f42e5c72ca9d
Daniel <60440770+AgniaInBloom@users.noreply.github.com>
2024-01-29 14:56:53 +0200

    Update and rename clean_install.sh to clean_install_2.sh

--- clean_install_2.sh
+++ clean_install_2.sh
@@ -1,21 +1,5 @@
-#yum update -y
-#sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-#service iptables stop
-yum install -y oracle-database-server-12cR2-preinstall
-mkdir -p /u01/app/oracle/product/12.2.0.1
-chown -R oracle:oinstall /u01
-chmod -R 775 /u01
-#cat /dev/null > /etc/hosts
-
-#cat > /etc/hosts <<EOF
-#127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 pandora.krenger.local
-#::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-#EOF
-
-su oracle
-
 cd /home/oracle
-
+echo $USER
 cat /dev/null > vi .bash_profile
 
 cat > .bash_profile <<EOF 
@@ -100,3 +84,5 @@ cat > /u01/app/oracle/product/12.2.0.1/dbhome_1/network/admin/samples/listener.o
 EOF
 
 ./netca -silent -responsefile /u01/app/oracle/product/12.2.0.1/dbhome_1/database/response/netca.rsp
+
+
